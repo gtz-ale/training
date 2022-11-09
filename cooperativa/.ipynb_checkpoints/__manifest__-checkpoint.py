e21d5c2cfdf53e221206a42e0ba09d53de1b9109
gtz-ale <alejandragutierrez@unifrut.com.mx>
2022-11-09 20:13:15 +0000

    []Cooperativa

---
+++ cooperativa/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,32 @@
+# -*- coding utf-8 -*-
+{
+    'name': 'Cooperativa Exercise',
+    'summary': """Training""",
+    'description': """
+    
+    Control de Cooperativa:
+    -Mercancias
+    -Punto de Venta
+    -Distribucion
+    
+    """,
+    
+    'author': 'Unifrut',
+    
+    'website': 'unifrut.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo':[
+        
+    ],
+    'license': 'OPL-1',
+}
--- cooperativa/__manifest__.py
+++ cooperativa/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding utf-8 -*-
 {
-    'name': 'Cooperativa',
+    'name': 'Cooperativa Exercise',
     'summary': """Training""",
     'description': """
     
