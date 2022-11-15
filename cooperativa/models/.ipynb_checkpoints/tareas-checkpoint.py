e29f214970ef07bc2640cd1cb43bf7135e6f345b
gtz-ale <alejandragutierrez@unifrut.com.mx>
2022-11-15 21:56:53 +0000

    []Cooperativa

--- cooperativa/models/.ipynb_checkpoints/tareas-checkpoint.py
+++ cooperativa/models/.ipynb_checkpoints/tareas-checkpoint.py
@@ -28,15 +28,15 @@ class Cooperativa(models.Model):
     status = fields.Char(string="Estado", default='Borrador')
     lider = fields.Char(string="Lider", default='')
     
-    @api.onchange('status', 'lider')
-    def _onchange_status(self):
-                if self.lider == '':
-                    raise UserError('El lide no puede estar vacio')
+    #@api.onchange('status', 'lider')
+    #def _onchange_status(self):
+     #           if self.status == 'Borrador':
+      #              raise UserError('La Tarea ahora esta lista para su ejecucion')
             
-                self.status = 'Listo'
+       #         self.status = 'Listo'
                 
-    @api.constrains('lider')
-    def _check_lider(self):
-        for record in self:
-            if record.lider == '':
-                raise ValidationError('Debe asignar un Lider a la Tarea')
+    #@api.constrains('lider')
+    #def _check_lider(self):
+     #   for record in self:
+      #      if record.lider == '':
+       #         raise ValidationError('Debe asignar un Lider a la Tarea')
